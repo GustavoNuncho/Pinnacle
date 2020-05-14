da5dccd72d392ffd5fac1a3cb83c038d25586ec2
Brandon <005275435@coyote.csusb.edu>
2020-05-14 15:36:48 -0700

    Created Project for git with original package

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxisRaw("Horizontal"));
+
+        if (Input.GetButtonDown("Jump"))
+        { jump = true; }
+
+        //Needs Animation
+        if (Input.GetButtonDown("Crouch"))
+        { crouch = true; }
+        else if(Input.GetButtonUp("Crouch"))
+        { crouch = false; }
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
