0d6bd98a4ef4261e7afcbf0862fa5b5155e84fe0
Brandon <005275435@coyote.csusb.edu>
2020-05-16 03:46:37 -0700

    Added my scene and library for Dictionary Funcs.

---
+++ Assets/Scripts/Enemy AI/ShortRangeEnemy/ShortRangeEnemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+
+public class ShortRangeEnemy : MonoBehaviour
+{
+
+    public float lookRadius;
+
+    public StateMachine  stateMachine =>  GetComponent<StateMachine>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+
+
+    private void InitializeStateMachine(){
+        var states = new Dictionary<Type, BaseState>()
+        {
+            {typeof(WanderState), new WanderState(this)}
+
+        };
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position,  lookRadius);
+        
+    }
+
+
+
+   public void DestroyEnemy()
+   {
+       Destroy(this.gameObject, 4.0f);
+   }
+
+
+}
+
---
+++ Assets/Scripts/Enemy AI/ShortRangeEnemy/WanderState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class WanderState : BaseState
+{
+    private ShortRangeEnemy _enemy;
+
+    
+
+    public WanderState(ShortRangeEnemy enemy) : base(enemy.gameObject)
+    {
+        _enemy = enemy;
+    }
+
+
+   
+    
+    
+    public override Type Tick(){
+        return null;
+
+    }
+    
+
+
+}
