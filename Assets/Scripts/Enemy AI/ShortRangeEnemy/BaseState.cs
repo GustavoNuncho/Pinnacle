0c5afb04e734b0ba852f420096aadb9d475a8783
Brandon <005275435@coyote.csusb.edu>
2020-06-02 20:41:35 -0700

    ShortRange Enemy AI

--- Assets/Scripts/Enemy AI/ShortRangeEnemy/BaseState.cs
+++ Assets/Scripts/Enemy AI/ShortRangeEnemy/BaseState.cs
@@ -8,15 +8,14 @@ public abstract class BaseState
     
     public BaseState(GameObject gameObject)
     {
-        this.gameObject = gameObject;
-        this.transform = gameObject.transform;
+        this.gameObject =   gameObject;
+        this.transform  =   gameObject.transform;
         
     }
         protected GameObject gameObject;
         protected Transform transform;
 
-
-
+    
         public abstract Type Tick();
     
 }
