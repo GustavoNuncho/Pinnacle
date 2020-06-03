1e98a7ebd59395e85c87a858a3741960935d098b
Jake Richardson <005688081@coyote.csusb.edu>
2020-06-03 03:01:23 -0700

    Many new additions more tomorrow

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,30 +11,37 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
-    
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-
         
-
-        //Needs Animation
         if (Input.GetButtonDown("Crouch"))
-        { crouch = true; }
+        { crouch = true;
+          //animation, we have none;
+        }
         else if(Input.GetButtonUp("Crouch"))
         { crouch = false; }
 
         if (Input.GetButtonDown("Jump"))
-        { jump = true;
-            //animator.jump = true; 
+        {
+            jump = true;
+            animator.SetBool("isJumping", true);
         }
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
     }
 
     private void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
+    }
+
+    public void OnLanding ()
+    {
+        animator.SetBool("isJumping", false);
     }
 }
